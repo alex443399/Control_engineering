92900ceb8584097674f378f49df0ffe25e0d70fb
alex443399 <alex443399@gmail.com>
2025-04-16 13:30:31 +0200

    Graphs for the report, a lot of processing and plotting

--- Feedforward/FFanalysis.m
+++ Feedforward/FFanalysis.m
@@ -2,19 +2,28 @@ addpath('Model')
 addpath('Measurements')
 clear all
 clc
-%% Load FF (Feed_forward_measurements_for_report.mat)
-error_ff0 = measurements_ff0(:,1);
-error_ff1 = measurements_ff1(:,1);
-error_ff2 = measurements_ff2(:,1);
-error_ff3 = measurements_ff3(:,1);
 %%
-plot(error_ff0(45537:117538))
+load("Feed_forward_measurements_for_report.mat")
+%% Load FF (Feed_forward_measurements_for_report.mat)
+% We select the correct time samples
+error_ff0 = measurements_ff0(45537:117538,1);
+error_ff1 = measurements_ff1(45583:117584,1);
+error_ff2 = measurements_ff2(45408:117409,1);
+error_ff3 = measurements_ff3(110323:182332,1);
+%%
+figure;
+Ts = 1/4000;
+plot((1:length(error_ff0))*Ts,error_ff0)
 hold on
-plot(error_ff1(45583:117584))
+plot((1:length(error_ff1))*Ts,error_ff1)
 hold on
-plot(error_ff2(45408:117409))
+plot((1:length(error_ff2))*Ts,error_ff2)
 hold on
-plot(error_ff3(110332:182332))
+plot((1:length(error_ff3))*Ts,error_ff3)
 hold off
-legend('No FF', 'Coulomb', 'V', 'A')
+legend('No FF', 'Added coulomb friction term', 'Added velocity term', 'Added acceleration term')
 % [-0.00038 -0.005 -0.00002]
+xlabel('Time (s)')
+ylabel('error (rad)')
+title('Tuning the feedforward')
+%%
